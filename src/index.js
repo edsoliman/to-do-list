30b28205c28a5ec46c3161d03e3456e6b3c00038
Ed Soliman <e.georgesoliman@gmail.com>
2022-01-20 19:43:31 -0500

    conflict talk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDoListApp from './App';
 
+// aww shiii conflict alert
 
 ReactDOM.render(<ToDoListApp />, document.getElementById('root'));
 
